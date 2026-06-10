bcb43ab3354e0fc8fa3f153171f01194a6c2ddfe
Aparna <Aparnanaik2497@gmail.com>
2026-06-10 15:38:31 +0530

    All types and ticket system

---
+++ day3.py
@@ -0,0 +1,27 @@
+customer_name = "Priya"
+is_urgent = True
+is_verified = True
+is_resolved = False
+ticket_number = 657
+
+response_time = 2.5
+satisfaction_score = 8.7
+resolution_rate = 94.5
+
+print(response_time)
+print(satisfaction_score)
+
+assigned_agent = None
+resolution = None
+customer_feedback = None
+
+print(assigned_agent)
+print(resolution)
+print(is_urgent)
+
+
+print("AI deskmate ticket")
+print("customer: " + customer_name)
+print("Urgent: " +str(is_urgent))
+print("Response time: " + str(response_time))
+print("Agent assigned: " + str(assigned_agent))
