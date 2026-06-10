743f5c054a58d039008d5ed02366820a82d7d11a
Aparna <Aparnanaik2497@gmail.com>
2026-06-10 15:11:15 +0530

    Day2-variables and data types

--- day1_first.py
+++ day1_first.py
@@ -1,2 +1,9 @@
 print("Deskmate AI is starting...")
 print("Building the future..")
+
+customer_name="wison"
+priority="high"
+message="hello"
+ticket_number=123
+
+print(priority)
---
+++ day2.py
@@ -0,0 +1,9 @@
+customer_name = "wilson"
+priority = "high"
+message = "hello"
+ticket_number = 123
+
+print(message)
+print(ticket_number)
+
+print("customer " + customer_name + " raised a " + priority + " priority ticket. ticket number: "+str(ticket_number) +" with a message " + message)
