3d943cac0678afd8500ff9f165442108c4f98ea0
Aparna <Aparnanaik2497@gmail.com>
2026-06-10 21:40:23 +0530

    updated priority

--- day1_first.py
+++ day1_first.py
@@ -1,9 +1,9 @@
 print("Deskmate AI is starting...")
 print("Building the future..")
 
-customer_name="wison"
-priority="high"
-message="hello"
-ticket_number=123
+customer_name = "wison"
+priority = "low"
+message = "hello"
+ticket_number = 123
 
 print(priority)
